ac5aeb9c4cc660dd49e3e5f143cdae7290f09319
jesusrick <jesus.penaranda@maristak.net>
2019-09-26 12:26:58 +0200

    1,2,4

--- prueba.php
+++ prueba.php
@@ -1,8 +1,9 @@
-<?php
- 
-for($n=2; $n<=100; $n= $n+2){
- 
-    echo $n."</br>";
-}
- 
-?>
+		<?php
+			$suma = 0;
+			for($i=1; $i<=6; $i++) {
+				if ($i % 2 == 0) {
+					$suma = $suma + $i;
+				}
+			}
+			echo "La suma vale: $suma";
+		?>
